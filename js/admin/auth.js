09eeff6ac4835a370b74ea5ee345bd3cf44106f3
Abdullah-Al Noman <81487006+thenomandev@users.noreply.github.com>
2026-05-14 18:37:22 +0600

    Add files via upload

--- js/admin/auth.js
+++ js/admin/auth.js
@@ -4,13 +4,17 @@ signOut,
 onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
+function goLogin(){
+window.location.href = "../login.html";
+}
+
 onAuthStateChanged(auth,(user)=>{
 if(!user){
-window.location.href="../../login.html";
+goLogin();
 }
 });
 
 window.logout = async function(){
 await signOut(auth);
-window.location.href="../../login.html";
+goLogin();
 };
