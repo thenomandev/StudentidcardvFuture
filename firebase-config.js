bbb8b8d2e257e6c38e155069fa405229a936bce3
Abdullah-Al Noman <81487006+thenomandev@users.noreply.github.com>
2026-05-15 00:36:59 +0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBKK9rimThpuHcyZpHfP3bT3HgmfTsfWUo",
@@ -17,4 +16,3 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
